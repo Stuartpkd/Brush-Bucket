7f5199b388907390fce6f361bc5780364661353f
Stuartpkd <stuartparkerdesign@gmail.com>
2023-10-25 14:09:58 +0000

    Add view to home app to render index page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
+    """ A view to return the home index page """
+
     return render(request, 'home/index.html')
 
-# Create your views here.
+
