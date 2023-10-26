ef85e1938361d46ddef1985f13f9dc47a6e4657a
Stuartpkd <stuartparkerdesign@gmail.com>
2023-10-26 13:03:12 +0000

    Fix issue with BrushCategory model

--- brushes/models.py
+++ brushes/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 
-class BrushCatagory(models.Model):
+class BrushCategory(models.Model):
     name = models.CharField(max_length=200)
-    friendly_name = models.CharField(max_length=200, null=True, Blank=True)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Brush(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey(BrushCategory, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=3, decimal_places=2)
