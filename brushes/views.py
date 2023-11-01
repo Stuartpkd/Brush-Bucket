5e9adb0089b4d4743b97a999e1f02d3305b84ae6
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-01 16:47:52 +0000

    Add view for rendering brush details

--- brushes/views.py
+++ brushes/views.py
@@ -12,3 +12,15 @@ def all_brushes(request):
     }
 
     return render(request, 'brushes/brushes.html', context)
+
+
+def brush_detail(request, brush_id):
+    """ A view to show brush details """
+
+    brush = get_object_or_404(Brush, pk=brush_id)
+
+    context = {
+        'brush': brush,
+    }
+
+    return render(request, 'brushes/brush_detail.html', context)
