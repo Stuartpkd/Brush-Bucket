c6fbc1948ec59c26bb1911590bdae54bb19e24b2
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-02 12:06:46 +0000

    Create view too add search criteria for brushes

--- brushes/views.py
+++ brushes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Brush
 
 
@@ -6,9 +7,23 @@ def all_brushes(request):
     """ A view to show all brushes, including sorting and search queries """
 
     brushes = Brush.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any "
+                                        "search criteria!")
+                return redirect(reverse('products'))
+
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query))
+            brushes = brushes.filter(queries)
 
     context = {
         'brushes': brushes,
+        'search_term': query,
     }
 
     return render(request, 'brushes/brushes.html', context)
