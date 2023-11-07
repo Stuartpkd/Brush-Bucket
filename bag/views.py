788b279d3756f8bfc0c31833b733b8002970c7f6
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-07 15:00:28 +0000

    Add url and view for adding brushes to cart

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,24 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+from django.shortcuts import redirect
+
+def add_to_bag(request, brush_id):
+    """ Add a digital brush to the shopping bag """
+
+    bag = request.session.get('bag', {})
+
+    if brush_id in bag:
+        # The user already has this brush in the bag
+        # Can show a message to the user or prevent adding it again
+        pass
+    else:
+        # Set the quantity to 1 for each brush if not already in the bag
+        bag[brush_id] = 1
+
+    request.session['bag'] = bag
+
+    redirect_url = request.POST.get('redirect_url')
+    return redirect(redirect_url)
