af21e62627927edb8937346d2b66972fd7a89f5b
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-14 15:15:43 +0000

    Add a featured brush section to index

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from brushes.models import Brush
+import random
 
 
 def index(request):
-    """ A view to return the home index page """
+    """ A view to return the home index page with featured brushes """
 
-    return render(request, 'home/index.html')
+    all_brushes = list(Brush.objects.all())  
+    featured_brushes = random.sample(all_brushes, min(len(all_brushes), 12))  # Select 12 random brushes
+
+    context = {
+        'featured_brushes': featured_brushes,
+    }
+
+    return render(request, 'home/index.html', context)
