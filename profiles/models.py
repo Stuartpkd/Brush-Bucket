a43d4c121585ad7fe2ff7e5364b223139113ba1e
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-16 17:08:42 +0000

    Add user profile models to profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from brushes.models import Brush
+from checkout.models import Order
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    information and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    saved_brushes = models.ManyToManyField(Brush, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def purchase_history(self):
+        """
+        Returns the user's purchase history.
+        """
+        return Order.objects.filter(user=self.user)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
