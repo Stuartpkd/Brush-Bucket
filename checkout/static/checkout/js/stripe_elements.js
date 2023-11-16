34ae7691eaca67fac8637afc698c112c021a2c06
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-16 16:15:31 +0000

    Update cache data function in stripe js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -74,29 +74,9 @@ form.addEventListener('submit', function (ev) {
                 card: card,
                 billing_details: {
                     name: $.trim(form.full_name.value),
-                    phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
-                    address: {
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
-                    }
+                    email: $.trim(form.email.value)
                 }
-            },
-            shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value),
-                }
-            },
+            }
         }).then(function (result) {
             if (result.error) {
                 var errorDiv = document.getElementById('card-errors');
@@ -121,5 +101,6 @@ form.addEventListener('submit', function (ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
+
 });
