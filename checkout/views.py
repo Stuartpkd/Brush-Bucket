64c47411db2dc69581c8a3593ebd2b08159c0a47
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-16 12:49:51 +0000

    Update checkout view post method

--- checkout/views.py
+++ checkout/views.py
@@ -3,29 +3,66 @@ from django.contrib import messages
 from django.conf import settings
 from bag.contexts import bag_contents
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from brushes.models import Brush
 
 import stripe
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('brushes'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-   
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            # Remove other fields that are not needed for digital products
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, quantity in bag.items():
+                try:
+                    brush = Brush.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        brush=brush,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
+                except Brush.DoesNotExist:
+                    messages.error(request, (
+                        "One of the brushes in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment")
+            return redirect(reverse('brushes'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -35,7 +72,7 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,  
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
