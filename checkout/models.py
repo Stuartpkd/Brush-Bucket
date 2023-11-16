05abbdf412e3d116988d2ca2112cc038becc7499
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-16 17:11:47 +0000

    COnnect user profile model to order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.db.models import Sum
 from brushes.models import Brush
+from profiles.models import UserProfile
 import uuid
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
@@ -14,7 +18,8 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254,
+                                  null=False, blank=False, default='')
 
     def __str__(self):
         return self.order_number
