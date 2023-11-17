bab1a1b28e4e84fe667e2a63dd2a67d0dc76d53a
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-17 13:02:13 +0000

    Fix purchase history not displaying in profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from checkout.models import Order
 
 
 def profile(request):
     """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    orders = Order.objects.filter(user_profile=user_profile).order_by('-date')
 
-    print(profile.orders.all())
+    orders = Order.objects.all()
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'profile': user_profile,
+        'orders': orders,
     }
 
     return render(request, template, context)
+    
