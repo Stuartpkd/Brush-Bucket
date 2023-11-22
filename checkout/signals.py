3e1fcdab23dc51b7625ce3414f02f4188a8a3e8b
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-22 13:38:41 +0000

    Improve success toast styling

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,7 +1,10 @@
 from django.db.models.signals import post_save, post_delete
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
+from django.contrib import messages
 
 from .models import OrderLineItem
+
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
@@ -10,9 +13,20 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     instance.order.update_total()
 
+
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
     instance.order.update_total()
+
+
+@receiver(user_logged_in)
+def user_logged_in_receiver(sender, request, user, **kwargs):
+    messages.info(request, f'Welcome back, {user.username}! You have successfully signed in.')
+
+
+@receiver(user_logged_out)
+def user_logged_out_receiver(sender, request, user, **kwargs):
+    messages.info(request, 'You have successfully signed out.')
