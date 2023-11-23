adf04134d8a891aed72781a6f71cd88884187f1c
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-23 18:31:05 +0000

    Add login required to checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -6,11 +6,13 @@ from bag.contexts import bag_contents
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from brushes.models import Brush
+from django.contrib.auth.decorators import login_required
 
 import stripe
 import json
 
 
+@login_required
 @require_POST
 def cache_checkout_data(request):
     try:
@@ -28,6 +30,7 @@ def cache_checkout_data(request):
         return HttpResponse(content=e, status=400)
 
 
+@login_required
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
@@ -115,6 +118,7 @@ def checkout(request):
     return render(request, template, context)
 
 
+@login_required
 def checkout_success(request, order_number):
     """
     Handle successful checkouts for digital brush store orders
