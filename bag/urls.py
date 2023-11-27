65eda23a57d4d8d448faeaa7b2fe127e265738be
Stuartpkd <stuartparkerdesign@gmail.com>
2023-11-27 22:52:38 +0000

    Fix linting error in bag urls

--- bag/urls.py
+++ bag/urls.py
@@ -5,5 +5,6 @@ from .views import remove_from_bag
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<brush_id>/', views.add_to_bag, name='add_to_bag'),
-    path('remove_from_bag/<int:brush_id>/', remove_from_bag, name='remove_from_bag'),
+    path('remove_from_bag/<int:brush_id>/', remove_from_bag,
+         name='remove_from_bag'),
 ]
