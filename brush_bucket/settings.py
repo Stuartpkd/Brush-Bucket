ff32ab35131a2246bcd7a20b27103ee5b39fda6b
Stuartpkd <stuartparkerdesign@gmail.com>
2023-12-11 15:41:58 +0000

    Register rating model to admin panel

--- brush_bucket/settings.py
+++ brush_bucket/settings.py
@@ -218,7 +218,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'brushbucket@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_PORT = 587  
+    EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
--- brushes/admin.py
+++ brushes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Brush, BrushCategory
+from .models import Brush, BrushCategory, Rating
 
 
 class BrushesAdmin(admin.ModelAdmin):
@@ -15,5 +15,11 @@ class BrushCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'friendly_name')
 
 
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('brush', 'user', 'rating')
+    search_fields = ('brush__name', 'user__username')
+
+
 admin.site.register(Brush, BrushesAdmin)
 admin.site.register(BrushCategory, BrushCategoryAdmin)
+admin.site.register(Rating, RatingAdmin)
